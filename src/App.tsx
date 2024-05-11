78a7a7336ed314f389dc148c2a124fb57e6faa7e
Duan Lizcano <duanlizcano@gmail.com>
2024-05-11 12:55:02 -0500

    Create useStore function Create type State and Action Chore: Add language selection functionality to Google Translate app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import { useStore } from "./hooks/useStore";
 
 function App() {
+  const { fromLanguage, setFromLanguage } = useStore();
+
   return (
     <div>
       <h1> Google Translate by: Duan Lizcano</h1>
+      <button onClick={() => setFromLanguage("es")}>Cambiar a español</button>
+      {fromLanguage}
     </div>
   );
 }
